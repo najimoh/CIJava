1efabafb7fc6fcf8015482927839c4b4ed1626a2
Naji Abdelraziq <naji_abdelraziq@yahoo.com>
2017-01-29 01:47:28 +0200

    first commit

---
+++ src/main/java/com/naji/apps/CiJavaApp/App.java
@@ -0,0 +1,13 @@
+package com.naji.apps.CiJavaApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
