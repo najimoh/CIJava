9c457cd8b2cbd6f9e6646503c3db1e150a3774da
Naji Abdelraziq <naji_abdelraziq@yahoo.com>
2017-01-29 02:05:18 +0200

    trigger build scm

--- src/main/java/com/naji/apps/CiJavaApp/App.java
+++ src/main/java/com/naji/apps/CiJavaApp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Naji" );
+        System.out.println( "Hello World! Naji 123" );
     }
 }
